c68d19b81767d466bbf6dfaf9ac85a8b24d0f340
wertysas <johanerics@gmail.com>
2023-08-06 21:51:51 +0200

    All ordering and Gemv Dot product implemented

--- Assignments/Week1/C/Gemv_I_Dots.c
+++ Assignments/Week1/C/Gemv_I_Dots.c
@@ -2,11 +2,12 @@
 #define chi( i )  x[ (i)*incx ]         // map chi( i )  to array x
 #define psi( i )  y[ (i)*incy ]         // map psi( i )  to array y
 
-void Dots( int, const double *, int, const double *, int, double * );
+void Dots( int n, const double *x, int incx, const double *y, int incy, double *gamma);
 
 void MyGemv( int m, int n, double *A, int ldA,
            double *x, int incx, double *y, int incy )
 {
+// row vector i of A is given by A[i + ldA*j], j=0,...,n-1
   for ( int i=0; i<m; i++ )
-    Dots(  ,     ,     ,     ,       ,      );
+    Dots( n, &A[i], ldA, x, incx, &y[i]);
 }
